6c07ce2c07ef1d0c09c2f00085e17a5ecce14001
Dailleth <dailleth2003@hotmail.com>
2023-05-04 19:18:38 -0500

    feat: an update controller for password change was added

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ProfileModel;
+use Database\Class\FoodBank\Users;
 use Database\Class\FoodBank\ReadUsers;
 use LionSecurity\JWT;
 
@@ -11,20 +12,27 @@ class ProfileController {
 	private ProfileModel $profileModel;
     private object $jwt;
 
-	public function __construct() {
-		$this->profileModel = new ProfileModel();
-        $this->jwt = JWT::decode(JWT::get());
-	}
+    public function __construct() {
+      $this->profileModel = new ProfileModel();
+      $this->jwt = JWT::decode(JWT::get());
+  }
 
 
-	public function readProfile() {
-        return $this->profileModel->readProfileDB(
-            (new ReadUsers())->setIdusers($this->jwt->data->idusers)
-        );
-	}
+  public function readProfile() {
+    return $this->profileModel->readProfileDB(
+        (new ReadUsers())->setIdusers($this->jwt->data->idusers)
+    );
+    }
 
-	public function update() {
+  public function updatePassword() {
+    $update_password = $this -> profileModel->updatePasswordDB(
+        Users::formFields()
+    );
+    if ($update_password->status === "database-error") {
+        return error("Ocurrio un error al actualizar su contraseña");
+    }
 
-	}
+    return success("El usuario se actualizo correctamente");
+}
 
 }
